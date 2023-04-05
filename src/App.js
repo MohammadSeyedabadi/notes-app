46cc57f0a5973a49754d25c79ed2d98116097c72
mohammad <mo.se.12369@gmail.com>
2023-04-05 23:55:09 +0430

    #

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import Split from 'react-split'
 import { nanoid } from 'nanoid'
 // any time, any state changes like adding a new note, the entire App component get re-rendered
 export default function App() {
-    // we do () => so it doesn't reach into localStorage on every single re-render of the App component
-    const [notes, setNotes] = React.useState(
-        () => JSON.parse(localStorage.getItem("notes")) || []
-        //localStorage.get("notes") => getting (pulling) the item with key of notes
-        //if this localStorage.getItem("notes"))  doesnt exist it returns null which is a falsy value so then we have []
-        // because localStorage.get("notes") is a stringify value we use JSON.parse on it so it turn back to javascript
-    )
+  // we do () => so it doesn't reach into localStorage on every single re-render of the App component
+  const [notes, setNotes] = React.useState(
+    () => JSON.parse(localStorage.getItem('notes')) || []
+    //localStorage.get("notes") => getting (pulling) the item with key of notes
+    //if this localStorage.getItem("notes"))  doesnt exist it returns null which is a falsy value so then we have []
+    // because localStorage.get("notes") is a stringify value we use JSON.parse on it so it turn back to javascript
+  )
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ''
   )
   //in order to interact with the local storage, every time the notes array changes, we will want to set up a side effect in react
   React.useEffect(() => {
-    localStorage.setItem('notes', JSON.stringify(notes))//'notes'=> because we are updating notes inside of local storage... JSON.stringify(notes)=> to stringify the notes so my array can turn into a string and succsessfully save to local storage 
+    localStorage.setItem('notes', JSON.stringify(notes)) //'notes'=> because we are updating notes inside of local storage... JSON.stringify(notes)=> to stringify the notes so my array can turn into a string and succsessfully save to local storage
   }, [notes])
 
   function createNewNote() {
@@ -74,14 +74,14 @@ export default function App() {
     </main>
   )
 }
-//on evey single re-render of the App component,on the background it is iqnoring the the state that is trying to 
+//on evey single re-render of the App component,on the background it is iqnoring the the state that is trying to
 // re-initialize here, but if there is code such as running a console.log() or in our example getting something
 //from local storage localStorage.getItem, it is going to run that code again, even if it doesnt use the value as
 // its new initial state because it is maintaing that state elsewhere in the background
 //ReactJS created a way to fix this. it is call lazy state initialization.
 //All you have to do is, instead of providing a value, i can provide a function that returns a value
 // const [state, setState] = React.useState(
-//     function() { 
+//     function() {
 //         return console.log("State initialization")
 //     }
 // )
